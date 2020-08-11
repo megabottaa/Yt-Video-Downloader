7993f0f6b594740ad820a4c911d60e8aecf0bc0a
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-08-11 16:35:47 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -3,6 +3,6 @@ import os
 class Config(object):
     # get a token from @BotFather
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-    # The Telegram API things
+    # Your Telegram Id
     OWNER_ID = int(os.environ.get('OWNER_ID') or 0)
 
